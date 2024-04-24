e9763e6b5cdfda208da4eded644997ae404772fd
Abel Amarilla <abelamarilla51777@gmail.com>
2024-04-24 02:10:45 -0400

    NewModelv2

---
+++ main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+  const parrafos = document.querySelectorAll(".description");
+
+  let alturas = [];
+  let alturaMaxima = 0;
+
+  const aplicarAlturas = (function aplicarAlturas() {
+    parrafos.forEach((parrafo) => {
+      if (alturaMaxima == 0) {
+        alturas.push(parrafo.clientHeight);
+      } else {
+        parrafo.style.height = alturaMaxima + "px";
+      }
+    });
+    return aplicarAlturas;
+  })();
+  alturaMaxima = Math.max.apply(Math, alturas);
+
+  aplicarAlturas();
+});
